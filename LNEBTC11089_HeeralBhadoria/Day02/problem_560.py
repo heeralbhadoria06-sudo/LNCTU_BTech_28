8d88686bd56ee96af2cfc22b5c73d88fa788ea91
heeralbhadoria06-sudo <heeralbhadoria06@gmail.com>
2026-08-17 09:46:36 +0530

    Create problem_560.py

---
+++ LNEBTC11089_HeeralBhadoria/Day02/problem_560.py
@@ -0,0 +1,21 @@
+def subarraySum(nums, k):
+    prefix_sum = 0
+    count = 0
+
+    freq = {0: 1}
+
+    for num in nums:
+        prefix_sum += num
+
+        if prefix_sum - k in freq:
+            count += freq[prefix_sum - k]
+
+        freq[prefix_sum] = freq.get(prefix_sum, 0) + 1
+
+    return count
+
+
+nums = eval(input())
+k = int(input())
+
+print(subarraySum(nums, k))
