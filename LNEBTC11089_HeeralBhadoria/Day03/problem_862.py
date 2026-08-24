043c52055da3e441dbe4ab43f56efdc50ad69232
heeralbhadoria06-sudo <heeralbhadoria06@gmail.com>
2026-08-24 05:36:24 +0530

    Add function to find shortest subarray with sum >= k

---
+++ LNEBTC11089_HeeralBhadoria/Day03/problem_862.py
@@ -0,0 +1,19 @@
+from collections import deque
+def shortestSubarray(nums, k):
+    n = len(nums)
+    prefix = [0] * (n + 1)
+    for i in range(n):
+        prefix[i + 1] = prefix[i] + nums[i]
+    dq = deque()
+    ans = n + 1
+    for i in range(n + 1):
+        while dq and prefix[i] - prefix[dq[0]] >= k:
+            ans = min(ans, i - dq.popleft())
+        while dq and prefix[i] <= prefix[dq[-1]]:
+            dq.pop()
+        dq.append(i)
+    return ans if ans <= n else -1
+nums = list(map(int, input().split()))
+k = int(input())
+
+print(shortestSubarray(nums, k))
